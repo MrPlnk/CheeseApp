5f63c49ff5a02c520beb6b9c9d260f36eee6e8eb
MrPlnk <artemkr2211@gmail.com>
2025-02-25 11:57:58 +0700

    Реализована димнамическая генерация виджетов

--- app/src/main/java/com/example/cheeseapp/srceens/SpecificCheeses/Cheese1Screen.kt
+++ app/src/main/java/com/example/cheeseapp/srceens/SpecificCheeses/Cheese1Screen.kt
@@ -15,17 +15,25 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.cheeseapp.srceens.SpecificCheeses.DynamicComposable.RowElem_Text_TexField_Text
 
 @Composable
 fun Cheese1Screen(navController: NavController){
+    val dim = arrayListOf("л", "мл", "мл", "гр", "гр", "л")
+    val pair = arrayListOf("Молоко", "Кальций", "Фермент", "TCC-20", "Eyes", "MicroMilk")
+    val stateMap = rememberPersistentStateMap(pair)
+
     Scaffold(
         topBar = {
             @OptIn(ExperimentalMaterial3Api::class)
@@ -51,19 +59,24 @@ fun Cheese1Screen(navController: NavController){
                     modifier = Modifier.padding(10.dp)
                         .fillMaxWidth(),
                     textAlign = TextAlign.Center)
-                Row(modifier = Modifier.padding(10.dp)
-                    .fillMaxWidth()){
-                    val milk_textField = remember{mutableStateOf("Вводить сюда")}
-                    Text("Молоко", fontSize = 28.sp,
-                        modifier = Modifier.weight(3f))
-                    TextField(milk_textField.value, onValueChange = {milk_textField.value = it},
-                        modifier = Modifier.weight(1f), keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-
-                    )
-
+                for( i in pair.indices){
+                    RowElem_Text_TexField_Text(pair[i], stateMap[pair[i]]!!, dim[i])
                 }
             }
         }
     }
 
+}
+
+@Composable
+fun rememberPersistentStateMap(list: List<String>, defaultLabel: String = ""): MutableMap<String, MutableState<String>> {
+    val stateMap = remember { mutableStateMapOf<String, MutableState<String>>() }
+
+    list.forEach { key ->
+        if (stateMap[key] == null) {
+            stateMap[key] = rememberSaveable { mutableStateOf(defaultLabel) }
+        }
+    }
+
+    return stateMap
 }
